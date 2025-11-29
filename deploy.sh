140d9e3401bd79c1eccedce035a4b1f72610b2c4
Swapnil Khatate <119690071+swapnilkhatate@users.noreply.github.com>
2025-11-29 10:16:58 +0530

    Update admin username and password in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 az group create --name MyResourceGroup --location eastus
-az deployment group create --resource-group MyResourceGroup --template-file azuredeploy.json --parameters vmName=myVM adminUsername=atul adminPassword=Ethans@123
+az deployment group create --resource-group MyResourceGroup --template-file azuredeploy.json --parameters vmName=myVM adminUsername=swapnil adminPassword=Box@123456789
